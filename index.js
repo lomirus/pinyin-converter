b43ffdd2aabd6b5c736a11bb826956bc76a41260
lomirus <lomirus.dev@protonmail.com>
2023-10-24 14:35:47 +0000

    Deploying to gh-pages from @ lomirus/pinyin-converter@d22a0a74f865b666e102924577496fcac05939ae 🚀

---
+++ index.js
@@ -0,0 +1,16 @@
+// import init, { convert_pinyin_ascii } from "../pkg/pinyin_converter.js";
+import init, { convert_pinyin_ascii } from "./pkg/pinyin_converter.js";
+
+const inputBox = document.querySelector("#input");
+const outputBox = document.querySelector("#output");
+
+await init();
+
+inputBox.addEventListener('input', (e) => {
+    try {
+        outputBox.value = convert_pinyin_ascii(e.target.value, 1);
+        outputBox.classList.remove('error')
+    } catch {
+        outputBox.classList.add('error')
+    }
+})
